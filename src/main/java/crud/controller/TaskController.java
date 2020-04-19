2b092de702b858310dfbb15dd626b18d6822a80a
unknown <mat-wit100@wp.pl>
2020-04-19 20:10:37 +0200

    First Commit

---
+++ src/main/java/crud/controller/TaskController.java
@@ -0,0 +1,48 @@
+package crud.controller;
+
+import crud.domain.TaskDto;
+import crud.mapper.TaskMapper;
+import crud.service.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/v1/task")
+
+public class TaskController {
+    @Autowired
+    private DbService service;
+    @Autowired
+    private TaskMapper taskMapper;
+
+    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
+    public List<TaskDto> getTasks() {
+        return taskMapper.mapToTaskDtoList(service.getAllTasks());
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getTask", params = {"id"})
+    public TaskDto getTask(@RequestParam(value = "id") Long taskId) throws TaskNotFoundException {
+        return taskMapper.mapToTaskDto(service.getTask(taskId).orElseThrow(TaskNotFoundException::new));
+    }
+
+//    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask", params = {"id"})
+//    public void deleteTask(@RequestParam(value = "id") Long taskId) {
+//        service.deleteTask(taskId);
+//    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
+    public TaskDto updateTask(@RequestBody TaskDto taskDto) {
+        return taskMapper.mapToTaskDto(service.saveTask(taskMapper.mapToTask(taskDto)));
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createTask", consumes = APPLICATION_JSON_VALUE)
+    public void createTask(@RequestBody TaskDto taskDto){
+        service.saveTask(taskMapper.mapToTask(taskDto));
+    }
+
+}
